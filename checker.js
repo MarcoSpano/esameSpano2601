c6121faa28309f6c8323e9f7056d199e8989f649
MarcoSpano <marco.spano@studenti.unitn.it>
2018-01-26 11:02:55 +0100

    fase2

--- checker.js
+++ checker.js
@@ -11,9 +11,61 @@ function check(url, invocationParameters,  expectedResultData, expectedResultSta
         resultDataAsExpected: null
     }
 
+    var paraminvocation = "?";
 
+    inv = Object.keys(invocationParameters);
+    val = Object.values(invocationParameters);
+
+    for(i=0;i<inv.length;i++) {
+        paraminvocation += inv[i] + "="+ val[i] +"&";
+    }
+    
+    var resurl = takeData(url,paraminvocation);
+
+    var compare = compareResults(expectedResultData,resurl);
+
+    var statusurl = getstatus(url,paraminvocation);
+
+    var checkstatus;
+    
+    if(expectedResultStatus == statusurl) checkstatus = true;
+    else checkstatus = false;
+
+    var checkPassed;
+
+    if(checkstatus && compare) checkPassed = true;
+    else checkPassed = false;
+    
+    return {
+        urlChecked: url,
+        resultData: resurl,
+        resultStatus: checkstatus,
+        statusTestPassed: checkPassed,
+        resultDataAsExpected: compare};
 
 }
+
+function takeData(url,paraminvocation) {
+    return fetch(url+paraminvocation)
+    .then(body => {
+        return body.json();
+    })
+    .catch(error => {
+        console.log(error);
+    });
+}
+
+function getstatus(url,paraminvocation) {
+    return fetch(url+paraminvocation)
+    .then(function(res) {
+    //console.log(res.status);
+    return res.statusText;
+    })
+    .catch(error => {
+        console.log(error);
+    });
+}
+
 
 
 // funzione che confronta due oggetti semplici e verifica se actual contiene tutti gli attributi di expected, e se per
@@ -27,4 +79,4 @@ function compareResults(expected, actual) {
     return true
 }
 
-module.exports = check
+module.exports = {check};
